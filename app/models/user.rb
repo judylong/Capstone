ff74a5a8d719aac70dc1aa742985a20770561ba1
Judy Long <long.judy92@gmail.com>
2015-09-15 12:04:51 -0400

    user auth (session and user controllers, views; user model); add StaticPagesController and view)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,36 @@
 class User < ActiveRecord::Base
+  validates :email, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, uniqueness: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(user_params)
+    user = User.find_by_email(user_params[:email])
+    user.try(:is_password?, user_params[:password]) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
