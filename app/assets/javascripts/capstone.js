34ac9586939878cc11939f5e3b8a724e64590275
Judy Long <long.judy92@gmail.com>
2015-09-15 16:04:06 -0400

    add backbone-on-rails gem and serializeJSONlibrary

---
+++ app/assets/javascripts/capstone.js
@@ -0,0 +1,13 @@
+window.Capstone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Capstone.initialize();
+});
