0f9d8bd18bd13840c89b4e22b09b50c9b2910013
Judy Long <long.judy92@gmail.com>
2015-09-15 15:40:13 -0400

    add api controller and projects controller

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  class ApiController < ApplicationController
+    before_action :require_logged_in!
+
+    def require_logged_in!
+      unless logged_in?
+        render json: ["must be logged in"], status: :unauthorized
+    end
+  end
+end
