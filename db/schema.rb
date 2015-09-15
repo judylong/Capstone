854c5aa68b8a0cbc8bc23d2a4839d9456b1265b3
Judy Long <long.judy92@gmail.com>
2015-09-15 14:43:37 -0400

    add category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915162903) do
+ActiveRecord::Schema.define(version: 20150915183955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title",       null: false
