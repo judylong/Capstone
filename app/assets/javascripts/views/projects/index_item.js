9caa2afd89056a0763f81afaa2039e09ac5b8d21
Judy Long <long.judy92@gmail.com>
2015-09-16 11:41:11 -0400

    debug and refactor subview for projects index

---
+++ app/assets/javascripts/views/projects/index_item.js
@@ -0,0 +1,12 @@
+Capstone.Views.ProjectsIndexItem = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.model, 'sync', this.render)
+  },
+  template: JST['projects/index_item'],
+  class_name: "projects-index-item",
+  render: function() {
+    var content = this.template({project: this.model});
+    this.$el.append(content);
+    return this;
+  }
+});
--- app/assets/javascripts/views/projects/projects_index.js
+++ app/assets/javascripts/views/projects/projects_index.js
@@ -1,14 +1,22 @@
 Capstone.Views.ProjectsIndex = Backbone.CompositeView.extend({
   initialize: function() {
-    this.listenTo(this.collection, 'sync', this.render)
+    this.listenTo(this.collection, 'sync', this.render);
+    this.listenTo(this.collection, 'add', this.addProjectsIndexItem)
+    this.collection.each(this.addProjectsIndexItem.bind(this));
+    this.listenTo(this.collection, 'remove', this.removeProjectsIndexItem)
   },
   template: JST['projects/index'],
   render: function() {
-    this.collection.each(function(project) {
-      var content = this.template({project: project});
-      this.$el.append(content);
-    }.bind(this))
-
+    var content = this.template();
+    this.$el.html(content);
+    this.attachSubviews();
     return this;
+  },
+  addProjectsIndexItem: function(projectsIndexItem) {
+    var subview = new Capstone.Views.ProjectsIndexItem({model: projectsIndexItem});
+    this.addSubview('.projects-index', subview);
+  },
+  removeProjectsIndexItem: function(projectsIndexItem) {
+    this.removeModelSubview('.projects-index', projectsIndexItem)
   }
 });
