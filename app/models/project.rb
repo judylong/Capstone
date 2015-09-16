a93eeb2ebddfcde1cd1408fe132bb99be35c48be
Judy Long <long.judy92@gmail.com>
2015-09-16 16:37:22 -0400

    bug fix: project side of project-user(owner) association

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   # validates :goal, format: { with: /^[0-9]+$/, on: :create}
 
   belongs_to :owner,
-    class_name: "Project",
+    class_name: "User",
     foreign_key: "owner_id"
 
   belongs_to :category
