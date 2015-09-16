146ade12e2de6ea544836a7c7669ad2918f2817e
Judy Long <long.judy92@gmail.com>
2015-09-16 16:36:22 -0400

    update rails routes, include namespaced users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
 
-  resources :users
-  resource :session
+  resources :users, only: [:create, :new]
+  resource :session, only: [:create, :new, :destroy]
 
   namespace :api, defaults: { format: :json } do
     resources :projects
+    resources :users, only: :show
   end
 end
