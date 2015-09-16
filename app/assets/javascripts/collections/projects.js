d31df6bf403cc649cbc872206136107a94ce60da
Judy Long <long.judy92@gmail.com>
2015-09-16 12:08:12 -0400

    add getOrFetch to projects collection

--- app/assets/javascripts/collections/projects.js
+++ app/assets/javascripts/collections/projects.js
@@ -1,4 +1,19 @@
 Capstone.Collections.Projects = Backbone.Collection.extend({
   model: Capstone.Models.Project,
-  url: "/api/projects"
+  url: "/api/projects",
+  getOrFetch: function(id) {
+    var model = this.get(id);
+    if (model) {
+      model.fetch();
+    } else {
+      model = new Capstone.Models.Project({id: id});
+      this.add(model)
+      model.fetch({
+        error: function() {
+          this.remove(model)
+        }.bind(this)
+      })
+    }
+    return model
+  }
 });
