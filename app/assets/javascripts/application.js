1a1ba7875e92b87566d987a7a19676679e3121a2
Judy Long <long.judy92@gmail.com>
2015-09-15 23:00:32 -0400

    add composite view library to utils directory and update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require underscore
 //= require backbone
 //= require capstone
+//= require_tree ./utils
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
