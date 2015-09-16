83b69d58801c61b0f066b8103ff3ab5af0f143a3
Judy Long <long.judy92@gmail.com>
2015-09-16 12:30:24 -0400

    add class to project show, update project index item show view and project show view

--- app/assets/javascripts/views/projects/project_show.js
+++ app/assets/javascripts/views/projects/project_show.js
@@ -1,6 +1,6 @@
 Capstone.Views.ProjectShow = Backbone.View.extend({
   template: JST['projects/show'],
-  className: "project-show"
+  className: "project-show",
   render: function() {
     var content = this.template({project: this.model});
     this.$el.html(content);
