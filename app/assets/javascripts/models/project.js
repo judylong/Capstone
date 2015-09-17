2225e847c8392e87f0ba67774840d7bb0e103432
Judy Long <long.judy92@gmail.com>
2015-09-16 20:34:11 -0400

    add project-rewards backbone association (and parse) to project model

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,3 +1,16 @@
 Capstone.Models.Project = Backbone.Model.extend({
-  urlRoot: '/api/projects'
+  urlRoot: '/api/projects',
+  rewards: function() {
+    if (!this._rewards) {
+      this._lists = new Capstone.Collections.Rewards([], {project: this});
+    }
+    return this._lists
+  },
+  parse: function(response) {
+    if (response.rewards) {
+      this.rewards().set(response.rewards, {parse: true});
+      delete response.rewards;
+    }
+    return response;
+  }
 });
