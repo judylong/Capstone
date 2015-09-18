8e6e2145bd3d15fe268d5b3cb3932ea5a6e40185
Judy Long <long.judy92@gmail.com>
2015-09-18 15:22:11 -0400

    update root and application html to rely on a backbone view and template for the header

---
+++ app/assets/javascripts/views/shared/header.js
@@ -0,0 +1,26 @@
+Capstone.Views.Header = Backbone.View.extend({
+  initialize: function(options) {
+    this.listenTo(Capstone.currentUser, "logIn logOut", this.render);
+  },
+
+  events: {
+    "click #log-out-link": "logOut"
+  },
+
+  template: JST['shared/header'],
+
+  render: function() {
+    var content = this.template({currentUser: Capstone.currentUser});
+    this.$el.html(content);
+    return this;
+  },
+
+  logOut: function(e) {
+    e.preventDefault();
+    Capstone.currentUser.logOut({
+      success: function() {
+        Backbone.history.navigate("session/new", { trigger: true });
+      }
+    });
+  }
+});
