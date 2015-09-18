768654fb70ed07c29cfacf894e234a33496b3b33
Judy Long <long.judy92@gmail.com>
2015-09-18 15:04:44 -0400

    remove users and sessions controllers and add api namespaced users and sessions controllers

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,5 +4,24 @@ module Api
       @user = User.find(params[:id])
       render :show
     end
+
+    def index
+      @users = User.all
+      render :index
+    end
+
+    def create
+      @user = User.find(params[:id])
+      if @user.save
+        log_in(user)
+      else
+        render json: @user.errors.full_messages, status: unprocessable_entity
+      end
+    end
+
+    protected
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
   end
 end
