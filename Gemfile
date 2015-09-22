5897d56a329b9394e779a021a62947098515020a
Judy Long <long.judy92@gmail.com>
2015-09-22 18:42:26 -0400

    install gem and create migration for omniauth

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'paperclip'
 gem 'figaro'
 gem 'aws-sdk', '~> 1.6'
 
+gem 'omniauth-facebook'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922174833) do
+ActiveRecord::Schema.define(version: 20150922205829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,9 +69,12 @@ ActiveRecord::Schema.define(version: 20150922174833) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "name",                null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
   add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
 
 end
