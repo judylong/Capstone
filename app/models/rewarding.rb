62ac7ef7581de32da4ea43e90234bda2d911ca49
Judy Long <long.judy92@gmail.com>
2015-09-24 20:56:34 -0400

    add validation to rewarding to prevent user from trying to claim a reward that has run out

--- app/models/rewarding.rb
+++ app/models/rewarding.rb
@@ -2,6 +2,7 @@ class Rewarding < ActiveRecord::Base
   validates :backer_id, :reward_id, presence: true
   validates :reward_backer, uniqueness: { scope: :reward }
   validate :check_unique_backer
+  validate :rewards_remaining
 
   belongs_to :reward_backer,
               class_name: "User",
@@ -15,6 +16,13 @@ class Rewarding < ActiveRecord::Base
     if reward_backer.backed_projects.exists?(project.id)
       errors.add(:backer_id, "user can only back once")
     end
-
   end
+
+  def rewards_remaining
+    debugger
+    if reward.attributes['limited_quantity'] && reward.reward_backers.count + 1 > reward.attributes['limited_quantity']
+      errors.add(:reward_id, "no more rewards left")
+    end
+  end
+
 end
