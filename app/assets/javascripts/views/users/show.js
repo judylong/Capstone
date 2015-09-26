9741731e4b9e87494730f6b17ed423614d005235
Judy Long <long.judy92@gmail.com>
2015-09-25 22:48:28 -0400

    change user show into composite view and render backed projects as subviews

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -1,11 +1,25 @@
-Capstone.Views.UserShow = Backbone.View.extend({
+Capstone.Views.UserShow = Backbone.CompositeView.extend({
   template: JST['users/show'],
   render: function() {
     var content = this.template({user: this.model});
     this.$el.html(content);
+    this.attachSubviews();
     return this;
   },
   initialize: function() {
-    this.listenTo(this.model, "sync", this.render)
+    this.listenTo(this.model, "sync", this.render);
+    this.model.backed_projects().each(this.addProjectItem.bind(this));
+    this.listenTo(this.model.backed_projects(), "add", this.addProjectItem);
+    this.listenTo(this.model.backed_projects(), "remove", this.removeProjectItem);
+  },
+
+  addProjectItem: function(projectItem) {
+    var subview = new Capstone.Views.ProjectsIndexItem({model: projectItem}).bind(this);
+    this.addSubview('.backed-projects-list', subview);
+  },
+
+  removeProjectItem: function(projectItem) {
+    this.removeModelSubview('.backed-projects-list', projectItem);
   }
+
 })
