b21f09dff771228b5a547083911c5aec158d7206
Judy Long <long.judy92@gmail.com>
2015-10-05 23:09:14 -0400

    make asset paths available for use via namespaced variables

--- app/assets/javascripts/capstone.js
+++ app/assets/javascripts/capstone.js
@@ -8,12 +8,19 @@ window.Capstone = {
     this.currentUser = new Capstone.Models.CurrentUser();
     this.currentUser.fetch();
 
-    Capstone.categories = categories
-    Capstone.shipping = shipping
-    Capstone.logo = $('.logo-url').html()
+    Capstone.categories = categories;
+    Capstone.shipping = shipping;
+    Capstone.logo = $('.logo-url').html();
+    Capstone.ninja = $('.ninja-url').html();
+    Capstone.townsville = $('.townsville-url').html();
+    Capstone.arrowLeft = $('.arrow-left-url').html();
+    Capstone.arrowRight = $('.arrow-right-url').html();
+    Capstone.hills = $('.hills-url').html();
+    Capstone.blueprint = $('.blueprint-url').html();
+
     this.header = new Capstone.Views.Header({el: "#header"});
-    this.footer = new Capstone.Views.Footer({el: "#footer"})
-    this.router = new Capstone.Routers.Users({$rootEl: $("#main")})
+    this.footer = new Capstone.Views.Footer({el: "#footer"});
+    this.router = new Capstone.Routers.Users({$rootEl: $("#main")});
     Backbone.history.start();
   }
 };
