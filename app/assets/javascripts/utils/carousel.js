3fa96f1bf559533cf5121e1e9b3507d811fb11c0
Judy Long <long.judy92@gmail.com>
2015-10-05 23:14:55 -0400

    add carousel utility

---
+++ app/assets/javascripts/utils/carousel.js
@@ -0,0 +1,73 @@
+Capstone.Carousel = function (el) {
+  this.$el = $(el);
+  this.$items = this.$el.find(".items").children();
+  this.activeIdx = 0;
+  this.transitioning = false;
+
+  this.$items.eq(0).addClass("active");
+
+  this.$el.on("click", "img.slide-left", this.slideLeft.bind(this));
+  this.$el.on("click", "img.slide-right", this.slideRight.bind(this));
+  this.$el.on("click", "li.carousel-circle", this.jumpSlide.bind(this));
+};
+
+Capstone.Carousel.prototype.jumpSlide = function(e) {
+  e.preventDefault();
+
+  if (this.transitioning || ($(e.currentTarget).data("id") === this.activeIdx)) {
+    return;
+  }
+  this.transitioning = true;
+
+  var $oldItem = this.$items.eq(this.activeIdx);
+  this.activeIdx = $(e.currentTarget).data("id");
+  var $newItem = this.$items.eq(this.activeIdx);
+
+  $newItem.addClass("active");
+  $oldItem.one("transitionend", (function () {
+    $oldItem.removeClass("active");
+    this.transitioning = false;
+  }).bind(this));
+
+  setTimeout((function () {
+    $oldItem.removeClass();
+  }).bind(this), 0);
+};
+
+Capstone.Carousel.prototype.slide = function (dir) {
+  if (this.transitioning) {
+    return;
+  }
+  this.transitioning = true;
+
+  var $oldItem = this.$items.eq(this.activeIdx);
+  this.activeIdx =
+    (this.activeIdx + dir + this.$items.length) % this.$items.length;
+  var $newItem = this.$items.eq(this.activeIdx);
+
+  $newItem.addClass("active");
+  $oldItem.one("transitionend", (function () {
+    $oldItem.removeClass("active");
+    this.transitioning = false;
+  }).bind(this));
+
+  setTimeout((function () {
+    $oldItem.removeClass();
+  }).bind(this), 0);
+};
+
+Capstone.Carousel.prototype.slideLeft = function (e) {
+  e.preventDefault();
+  this.slide(1);
+};
+
+Capstone.Carousel.prototype.slideRight = function (e) {
+  e.preventDefault();
+  this.slide(-1);
+};
+
+$.fn.carousel = function () {
+  return this.each(function () {
+    new Capstone.Carousel(this);
+  });
+};
